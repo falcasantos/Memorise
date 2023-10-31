cbbbc2a3199649c73c974f9cfeffb728cd7b51ac
Robert Falcasantos <robert@falcasantos.com>
2023-10-31 21:52:58 +0100

    Initial Commit

---
+++ Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by Robert Falcasantos on 2023-10-31.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
